fc8b91d1a4fb163c8c42ce14e3148cea423f759a
juansesu3 <juanseu3@hotmail.com>
2023-08-24 00:14:00 +0200

    problems with redirec our pages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,22 +1,20 @@
-/*import clientPromise from "@/lib/mongodb";
-import { Admin } from "@/models/admin";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-*/
-
 import { mongooseConnect } from "@/lib/mongoose";
 import { User } from "@/models/user";
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
-const authOptions = {
+const isAdminEmails = async (email) => {
+  return !!(await User.findOne({ email }));
+};
+
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {},
       async authorize(credentials) {
         const { email, password } = credentials;
-
         try {
           await mongooseConnect();
           const user = await User.findOne({ email });
@@ -45,55 +43,6 @@ const authOptions = {
 
 export default NextAuth(authOptions);
 
-/*
-
-
-const isAdminEmails = async (email) => {
-  return true;
-  return !!(await Admin.findOne({ email }));
-};
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {},
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        console.log(credentials);
-
-       
-        const res = await axios.post("/api/register", data);
-
-        const user = isAdminEmails(credentials.email);
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-  adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: async ({ session, token, user }) => {
-      if (await isAdminEmails(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    },
-  },
-};
-
-export default NextAuth(authOptions);
-
 export const isAdminRequest = async (req, res) => {
   const session = await getServerSession(req, res, authOptions);
 
@@ -103,4 +52,3 @@ export const isAdminRequest = async (req, res) => {
     throw "Not an admin";
   }
 };
-*/
